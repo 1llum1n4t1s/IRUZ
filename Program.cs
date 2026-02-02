1ef2ae6f23a46a98413b0b77ab5b933a32db3900
ゆろち <1llum1n4t1@duck.com>
2026-02-03 08:03:07 +0900

    不具合修正

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
-﻿using System;
+using System;
 using Avalonia;
+using Velopack;
 
 namespace IRU
 {
     internal sealed class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
+        /// <summary>
+        /// アプリケーションのエントリポイント。Velopack のブートストラップを最初に実行し、続けて Avalonia アプリを起動する。
+        /// </summary>
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            VelopackApp.Build().Run();
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
